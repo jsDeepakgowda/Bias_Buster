35752b1959765a740f62b058e824280deffbf638
Deepak J S <deepaksrinivas242@gmail.com>
2025-03-21 15:30:35 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import analyze_news, home
+from .views import *
 
 urlpatterns = [
     path("", home, name="home"),
+    path("analyse/", analyse, name="analyse"),
+    path("about/", about, name="about"),
     path("analyze-news/", analyze_news, name="analyze_news"),
 ]
